2173bd4b9347c4405afa8c695f4ddbe889281164
mahamadou-simpara <msimpara@doninya.com>
2023-07-15 20:40:34 +0000

    Slpit page website

---
+++ app.js
@@ -0,0 +1,23 @@
+const left = document.querySelector('.left-image');
+const right = document.querySelector('.right-image');
+
+console.log('Hello !');
+left.addEventListener("mouseenter", function() {
+    // Code to execute when the element is hovered
+
+    
+    right.classList.remove('expand');
+    left.classList.remove('restrict');
+
+    left.classList.add('expand');
+    right.classList.add('restrict');
+  });
+
+  right.addEventListener("mouseenter", function() {
+    // Code to execute when the element is hovered
+    left.classList.remove('expand');
+    right.classList.remove('restrict');
+
+    right.classList.add('expand');
+    left.classList.add('restrict');
+  });
